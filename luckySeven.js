5535d0da6a5da7594608bea68c80ab286941474a
Keely Brennan <keelybrennan4@gmail.com>
2019-12-22 20:55:37 -0600

    finished game logic

--- luckySeven.js
+++ luckySeven.js
@@ -5,14 +5,15 @@ function rollDice(){
         
 function playGame(){
     var roundCounter = 0;
-    
     var maxMoney = 0;
-    var roundMaxMoney = 0;
-    var winCount = 0;
+    var roundMaxMoney = 1;
+
     // get starting bet from user and check if starting bet is greater than 0
     var bet =  document.getElementById("bet").value;
     //set initial bet equal to user's money
     var money = bet;
+    var betsArray = [];
+    
     console.log("Your initial bet is $" + bet);
 
     if (money <= 0) {
@@ -27,44 +28,34 @@ function playGame(){
             roundCounter++;
             console.log("Dice Sum: " + diceSum);
         
+
             //if dice sum is not 7, user loses round and money is decremented by $1
             if(diceSum != 7){
                 money--;
+                betsArray.push(money);
                 console.log("Not a winner this round, " + roundCounter + "|  money: $" + money);
 
             //if dice sum is 7, user wins round and money is incremented by $4
             } else {
                 money += 4;
-                //winCount++
-                //winCount[winCount.length] = money;
-                //console.log("win count" + winCount);
-                //console.log("You won this round, " + roundCounter +  "|  money: $" + money);
+                betsArray.push(money);
+                console.log("You're a winner this round, " + roundCounter + "|  money: $" + money);
             }
-
-                //also check if round qualifies for max winning
-                if (money > maxMoney) {
-                   //roundCounter = roundMaxMoney;
-                   console.log(roundMaxMoney);
-                }
-
-                 //   maxMoney += (money - maxMoney);
-                 //   roundMaxMoney = roundCounter;
-                //}
-                //    console.log("Max Money: " + roundMaxMoney + " " + roundCounter);
-                //    console.log("YOU WON!")åß
-        
+            //push money and roll count to bets Array to determine highest amount of money held, and at which round
+            var maxMoney = Math.max.apply(Math, betsArray);
+            var roundMaxMoney = betsArray.indexOf(maxMoney) + 1; //add one to round count since index counting starts at 0
         }
+        console.log("Highest Amount Won: $" + maxMoney + "| Round: " + roundMaxMoney);
     }
     //display results on page
     document.getElementById("heading").innerHTML = ("Results");
     document.getElementById("results").style.display = "block";
 	document.getElementById("startingBet").innerHTML = ("$" + bet + ".00");
 	document.getElementById("rollsBeforeBroke").innerHTML = roundCounter;
-    document.getElementById("maxMoney").innerHTML = ("$" + roundMaxMoney + ".00");
-    document.getElementById("highRollCount").innerHTML = "?"
-    //document.getElementById("highRollCount").innerHTML = winCount.indexOf(roundMaxMoney);
-	//document.getElementById("submit").innerHTML = "Play Again?";
-    //document.forms["inputBet"]["bet"];
+    document.getElementById("maxMoney").innerHTML = ("$" + maxMoney + ".00");
+    document.getElementById("highRollCount").innerHTML = roundMaxMoney;
+    document.getElementById("submit").innerHTML = "Play Again?";
+    
     return false;
 }
 
